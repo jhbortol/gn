62044bdb43670cc6cd965275a7b60b0309877028
jhbortol <jhbortol@gmail.com>
2026-02-12 14:28:46 +0000

    fix: correct API host URL by removing hyphen in funcguianoivasdev

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
-  API_BASE_URL: 'https://func-guianoivas-dev-deczg2affxb9f7f7.brazilsouth-01.azurewebsites.net/api/v1',
+  API_BASE_URL: 'https://funcguianoivasdev-deczg2affxb9f7.brazilsouth-01.azurewebsites.net/api/v1',
   INTERNAL_AUTH_EMAIL: 'jhbortol@gmail.com',
   INTERNAL_AUTH_PASSWORD: 'pesc01',
   FORNECEDOR_PUBLICADO: null as boolean | null,

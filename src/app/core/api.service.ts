1c8efc56a14d06267efdc41efed5940747f04d28
jhbortol <jhbortol@gmail.com>
2026-02-19 11:45:11 +0000

    Implement build-time API_BASE_URL replacement for Azure Static Web Apps

--- src/app/core/api.service.ts
+++ src/app/core/api.service.ts
@@ -1,18 +1,11 @@
-import { Injectable, Inject, PLATFORM_ID } from '@angular/core';
+import { Injectable } from '@angular/core';
 import { HttpClient, HttpParams } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { environment } from '../../environments/environment';
-import { isPlatformServer } from '@angular/common';
 
 @Injectable({ providedIn: 'root' })
 export class ApiService {
-  private apiBaseUrl: string;
-
-  constructor(private http: HttpClient, @Inject(PLATFORM_ID) private platformId: Object) {
-    this.apiBaseUrl = isPlatformServer(this.platformId) && process.env['API_BASE_URL']
-      ? process.env['API_BASE_URL']
-      : environment.API_BASE_URL;
-  }
+  constructor(private http: HttpClient) {}
 
   get<T>(url: string, params?: Record<string, any>): Observable<T> {
     let httpParams = new HttpParams();
@@ -21,10 +14,10 @@ export class ApiService {
         if (v !== undefined && v !== null) httpParams = httpParams.set(k, String(v));
       });
     }
-    return this.http.get<T>(`${this.apiBaseUrl}${url}`, { params: httpParams });
+    return this.http.get<T>(`${environment.API_BASE_URL}${url}`, { params: httpParams });
   }
 
   post<T>(url: string, body: any): Observable<T> {
-    return this.http.post<T>(`${this.apiBaseUrl}${url}`, body);
+    return this.http.post<T>(`${environment.API_BASE_URL}${url}`, body);
   }
 }
